0e9acf4010d5ec32ae20f5ca11e70c9e4c64f272
ishaan2020 <53574413+ishaan2020@users.noreply.github.com>
2021-08-22 09:26:30 -0400

    small change

--- commerce/settings.py
+++ commerce/settings.py
@@ -14,6 +14,8 @@ import os
 import django_heroku
 from decouple import config
 
+import dj_database_url
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -85,6 +87,11 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES["default"].update(db_from_env)
+
 
 AUTH_USER_MODEL = 'auctions.User'
 
